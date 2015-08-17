e7bae28921dbd3ebf7f3b58eeec531b82e117d74
AmericaGL <info@cybercodetwins.com>
2015-08-17 15:50:59 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Your JS goes here
 window.onload = function(){
 addElement();
+addColors();
 // addMore();
 
 };//window onload
@@ -10,28 +11,41 @@ addElement();
 //add style to the div
 function addElement(){
 var newDiv = document.createElement("div");
-// newDiv.style.backgroundColor = "red";
 newDiv.style.width="11.1%";
 newDiv.style.height="11.1%";
 newDiv.style.float="left";
 newDiv.style.paddingBottom="11.1%";
-
 return newDiv;
-} 
+}
+
+function addColors(){
+
+var r = Math.floor(Math.random()*255);
+var g = Math.floor(Math.random()*255);
+var b = Math.floor(Math.random()*255);
+var newColor = randomDiv.style.backgroundColor= 'rgb(' + r + "," + g + "," + b +')';
+return newColor;
+}
+
+
+//"rgb(111,111,111)";
+//var rgb = r + " " + g + " " + b + " "; "rgb(" + r + "," + 
+// var hi = "HEY"
+// hi + " america"
+
+// return  newColor;
+
+
+ 
 //call local var by calling its function return
 //calling function addElement return answer which become new div which 
 //becomes document.createElement("div")
 for (var i=1; i<=63; i++) {
 	var randomDiv = addElement();
+	var randomColor = addColors();
 	//adds local var to the outside but not one times but many times
 	//function addElement returns newDiv which is document.createElement("div");
 
-	if( i % 2 === 0 ){
-		randomDiv.style.backgroundColor="red";
-	}
-	else{
-		randomDiv.style.backgroundColor="black";
-	}
 	//you can define a function outside of for loop to attach it to the body 
 	//whatever happens it will be assign this variable. You don't want this 
 	//to be in the function addElement though in other body to body(not good).
@@ -41,30 +55,5 @@ for (var i=1; i<=63; i++) {
 
 }
 
-// function test()
-// var a = "b";
-// return a;
-// }
-
-// var c = test();
-// var c = a
-// var c = "b"
-
-//you need to multiply the div 64 times by adding an array
-//
-//
-//  var newDiv=document.createElement("div");
-//  document.body.appendChild(newDiv);
-//  for (var i = 1;
-//    i <= 64; i++) {
-//    	if( i % 2 === 0){
-//    		newDiv.style.backgroundColor = "red";
-//    		 newDiv.style.width="300px";
-//    		 newDiv.style.height="300px";
-//    	}
-//    	else
-//    		newDiv.style.backgroundColor="black";
-//    		 newDiv.style.width="300px";
-//    		 newDiv.style.height="300px";
-// }
-// // }
+//Math floor help round the number 1.6 downward to it's nearest integer
+//math random returns a 
