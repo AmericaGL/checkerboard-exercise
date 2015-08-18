9c8a0346206aca5131e1f33f4adee0d16c748681
AmericaGL <info@cybercodetwins.com>
2015-08-18 04:53:29 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // Your JS goes here
 window.onload = function(){
+window.setTimeout(function(){document.location.reload(true);}, 2000);
+
 addElement();
 addBigDiv();
 // addMore();
 
 };//window onload
+function addColors(){
+
+var r = Math.floor(Math.random()*255);
+var g = Math.floor(Math.random()*255);
+var b = Math.floor(Math.random()*255);
+var newColor = randomDiv.style.backgroundColor= 'rgb(' + r + "," + g + "," + b +')';
+return newColor;
+}
 
 
 function addElement(){
@@ -23,21 +33,22 @@ return newBottomDiv;
 //becomes document.createElement("div")
 for (var i=1; i<=63; i++) {
 	var randomDiv = addElement();
+	var randomColor= addColors();
 
 //	var megaDiv = addBigDiv();
 	//adds local var to the outside but not one times but many times
 	//function addElement returns newDiv which is document.createElement("div");
 
-	 if( i % 2 === 0 ){
-	 	randomDiv.style.backgroundColor="red";
-	 	randomDiv.style.opacity=".9";
-	 	randomDiv.style.zIndex="";
-	 }
-	 else{
-	 	randomDiv.style.backgroundColor="black";
-	 	randomDiv.style.opacity=".9";
-	 	randomDiv.style.zIndex="";
-	 }
+	 // if( i % 2 === 0 ){
+	 // 	randomDiv.style.backgroundColor="red";
+	 // 	randomDiv.style.opacity=".9";
+	 // 	randomDiv.style.zIndex="";
+	 // }
+	 // else{
+	 // 	randomDiv.style.backgroundColor="black";
+	 // 	randomDiv.style.opacity=".9";
+	 // 	randomDiv.style.zIndex="";
+	 // }
 	//you can define a function outside of for loop to attach it to the body 
 	//whatever happens it will be assign this variable. You don't want this 
 	//to be in the function addElement though in other body to body(not good).
