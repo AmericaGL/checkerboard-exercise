bc251b0c357f82532092191418cd9238d786118e
AmericaGL <info@cybercodetwins.com>
2015-11-05 09:45:14 -0800

    changing opacity

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ addBigDiv();
 
 function addElement(){
 var newBottomDiv = document.createElement("div");
-//newDiv.style.background="linear-gradient(135deg,red,red 60%,blue)";
+
 newBottomDiv.style.width="11.1%";
 newBottomDiv.style.height="11.1%";
 newBottomDiv.style.float="left";
 newBottomDiv.style.paddingBottom="11.1%";
 //newBottomDiv.style.backgroundColor="linear-gradient(45deg, #ed1c24, #fcee21)";
-
+//newDiv.style.background="linear-gradient(135deg,red,red 60%,blue)";
 return newBottomDiv;
 } 
 //call local var by calling its function return
@@ -24,23 +24,23 @@ return newBottomDiv;
 for (var i=1; i<=63; i++) {
 	var randomDiv = addElement();
 
-//	var megaDiv = addBigDiv();
-	//adds local var to the outside but not one times but many times
-	//function addElement returns newDiv which is document.createElement("div");
+//var megaDiv = addBigDiv();
+//adds local var to the outside but not one times but many times
+//function addElement returns newDiv which is document.createElement("div");
 
 	 if( i % 2 === 0 ){
 	 	randomDiv.style.backgroundColor="red";
-	 	randomDiv.style.opacity=".9";
+	 	randomDiv.style.opacity="";
 	 	randomDiv.style.zIndex="";
 	 }
 	 else{
 	 	randomDiv.style.backgroundColor="black";
-	 	randomDiv.style.opacity=".9";
+	 	randomDiv.style.opacity="";
 	 	randomDiv.style.zIndex="";
 	 }
-	//you can define a function outside of for loop to attach it to the body 
-	//whatever happens it will be assign this variable. You don't want this 
-	//to be in the function addElement though in other body to body(not good).
+//you can define a function outside of for loop to attach it to the body 
+//whatever happens it will be assign this variable. You don't want this 
+//to be in the function addElement though in other body to body(not good).
 	document.body.appendChild(randomDiv);
 	randomDiv.style.position="relative";
 
@@ -55,13 +55,13 @@ for (var i=1; i<=63; i++) {
    		
    	}
    	else
-   		newDiv.style.background="linear-gradient(45deg, #ed1c24, #fcee21)";
+   		newDiv.style.background="linear-gradient(25deg, #ed1c24, #fcee21)";
    		newDiv.style.position="relative";
    		 newDiv.style.width="100%";
    		 newDiv.style.height="100%";
-   		 newDiv.style.paddingBottom="78%";
-   		 newDiv.style.opacity=".8";
-   		 newDiv.style.zIndex="";
+   		 newDiv.style.paddingBottom="77.8%";
+   		 newDiv.style.opacity=".6";
+   		 newDiv.style.zIndex="1";
    		
 }
 // }
